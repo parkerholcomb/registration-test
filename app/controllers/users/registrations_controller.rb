3eb6f6f2efb80bfefd4a4e6417e593628f49c896
Parker Holcomb <parker.holcomb@gmail.com>
2016-01-04 14:21:51 -0500

    set routes to use devise controllers, permit params, route to root page after sign

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,9 +39,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name, :last_name, :mobile_number, :howd_you_hear,
+                                                      :password, :password_confirmation, :email)}
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -49,9 +51,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
