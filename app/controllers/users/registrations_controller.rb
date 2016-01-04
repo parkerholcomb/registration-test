2a0efda9fc59e1413f534fa57f1ea79165c84666
Parker Holcomb <parker.holcomb@gmail.com>
2016-01-04 17:58:58 -0500

    to-do is set up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,9 +3,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+    flash[:notice] = "this is a dismissable flash notice"
+  end
 
   # POST /resource
   # def create
